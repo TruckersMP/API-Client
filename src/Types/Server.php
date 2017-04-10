58bd1d233c1f2a5e050581a17463e081048153f5
Max Mekenya <cjmaxik@users.noreply.github.com>
2017-04-10 19:43:58 +1000

    Added new rows

--- src/Types/Server.php
+++ src/Types/Server.php
@@ -58,7 +58,7 @@ class Server
      * @var int
      */
     public $queue;
-    
+
     /**
      * Max player count
      * @var int
@@ -72,22 +72,56 @@ class Server
     public $speedLimiter;
 
     /**
+     * Collisions
+     * @var bool
+     */
+    public $collisions;
+
+    /**
+     * Will cars be available for all players
+     * @var bool
+     */
+    public $carsForPlayers;
+
+    /**
+     * Will police cars be available for all players
+     * @var bool
+     */
+    public $policeCarsForPlayers;
+
+    /**
+     * "Away from keyboard" status
+     * @var bool
+     */
+    public $afkEnabled;
+
+    /**
+     * Sync delay (tick rate)
+     * @var bool
+     */
+    public $syncDelay;
+
+    /**
      * Server constructor.
      * @param $server
      */
     public function __construct($server)
     {
-        $this->id           = intval($server['id']);
-        $this->game         = $server['game'];
-        $this->ip           = $server['ip'];
-        $this->port         = intval($server['port']);
-        $this->name         = $server['name'];
-        $this->shortName    = $server['shortname'];
-        $this->online       = boolval($server['online']);
-        $this->players      = intval($server['players']);
-        $this->queue        = intval($server['queue']);
-        $this->maxPlayers   = intval($server['maxplayers']);
-        $this->speedLimiter = boolval($server['speedlimiter']);
-
+        $this->id                   = intval($server['id']);
+        $this->game                 = $server['game'];
+        $this->ip                   = $server['ip'];
+        $this->port                 = intval($server['port']);
+        $this->name                 = $server['name'];
+        $this->shortName            = $server['shortname'];
+        $this->online               = boolval($server['online']);
+        $this->players              = intval($server['players']);
+        $this->queue                = intval($server['queue']);
+        $this->maxPlayers           = intval($server['maxplayers']);
+        $this->speedLimiter         = boolval($server['speedlimiter']);
+        $this->collisions           = boolval($server['collisions']);
+        $this->carsForPlayers       = boolval($server['carsforplayers']);
+        $this->policeCarsForPlayers = boolval($server['policecarsforplayers']);
+        $this->afkEnabled           = boolval($server['afkenabled']);
+        $this->syncDelay            = intval($server['syncdelay']);
     }
 }
