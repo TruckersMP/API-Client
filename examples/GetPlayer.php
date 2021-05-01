86bfe72e299cc099b02b830c9ea869525b12a8e8
Ben Sherred <ben@sherred.co.uk>
2021-05-01 12:56:12 +0100

    Use valid id in examples

--- examples/GetPlayer.php
+++ examples/GetPlayer.php
@@ -7,7 +7,7 @@ use TruckersMP\APIClient\Client;
 $client = new Client();
 
 try {
-    $player = $client->player(11111111111111)->get();
+    $player = $client->player(1111)->get();
     echo $player->getName();
 } catch (Exception $exception) {
     echo $exception->getMessage();
