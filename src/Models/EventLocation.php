e3a1b7f8675aea54744a582a2faa5a87a8e7a07b
Diego <me@diegor.nl>
2021-11-10 02:22:00 +0100

    Create EventLocation model

---
+++ src/Models/EventLocation.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace TruckersMP\APIClient\Models;
+
+class EventLocation
+{
+    /**
+     * The event location.
+     *
+     * @var string
+     */
+    protected $location;
+
+    /**
+     * The event city.
+     *
+     * @var string
+     */
+    protected $city;
+
+    /**
+     * Create a new EventLocation instance.
+     *
+     * @param string $location
+     * @param string $city
+     * @return void
+     */
+    public function __construct(
+        string $location,
+        string $city
+    )
+    {
+        $this->location = $location;
+        $this->city = $city;
+    }
+
+    /**
+     * Get the location.
+     *
+     * @return string
+     */
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+    /**
+     * Get the city.
+     *
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+}
